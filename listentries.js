01c81f9ff42845a07e7498196ee5104151a220e8
yanziology <yanziology@gmail.com>
2015-07-20 22:09:09 +0800

    Update listentries.js

--- listentries.js
+++ listentries.js
@@ -1,18 +1,19 @@
 function listentry(json) {
-document.write('');
+  document.write('<ul id="bloglist">');
 for (var i = 0; i < numposts; i++) {
-var entry = json.feed.entry[i];
-var posttitle = entry.title.$t;
-var posturl;if (i == json.feed.entry.length) break;
-for (var k = 0; k < entry.link.length;k++){
-if(entry.link[k].rel=='replies'&&entry.link[k].type=='text/html'){
-var commenttext=entry.link[k].title;
-var commenturl=entry.link[k].href;
-}
-if (entry.link[k].rel == 'alternate') {
-posturl = entry.link[k].href;break;
-}
-}
+  var entry = json.feed.entry[i];
+  var posttitle = entry.title.$t;
+  var posturl;if (i == json.feed.entry.length) break;
+  
+  for (var k = 0; k < entry.link.length;k++){
+    if(entry.link[k].rel=='replies'&&entry.link[k].type=='text/html'){
+      var commenttext=entry.link[k].title;
+      var commenturl=entry.link[k].href;
+    }
+    if (entry.link[k].rel == 'alternate') {
+      posturl = entry.link[k].href;break;
+    }
+  }
 /*var thumburl;
 try {
 thumburl=entry.media.url;
@@ -27,7 +28,9 @@ thumburl=d;} else thumburl='http://2.bp.blogspot.com/-fyukB_HLG_w/UbManI7ySAI/AA
 //document.write('<div class="posttitle">'+'<a href="'+posturl+'">'+posttitle+'</a>=123'+'</div>');
 //document.write('</span></li>'+'</ul>'+'</div>');
 
-document.write('<div>HERE: '+'<a href="'+posturl+'">'+posttitle+'</a>'+'</div>');
-//document.write('<div class="fix"></div>');
+  //document.write('<div>HERE: '+'<a href="'+posturl+'">'+posttitle+'</a>'+'</div>');
+  document.write('<li>'+'<a href="'+posturl+'">'+posttitle+'</a>'+'</li>');
+  //document.write('<div class="fix"></div>');
 }
+  document.write('</ul>');
 }
