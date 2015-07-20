1c55f652b5784d89274b93be7d9579890c39691b
yanziology <yanziology@gmail.com>
2015-07-20 21:47:19 +0800

    Update listentries.js

--- listentries.js
+++ listentries.js
@@ -23,7 +23,7 @@ thumburl=d;} else thumburl='http://2.bp.blogspot.com/-fyukB_HLG_w/UbManI7ySAI/AA
 }*/
 document.write('<div class="sitemap-container">'+'<ul id="bloglist">'+'<li>');
 //var towrite='';var flag=0;
-document.write('<div class="posttitle">'+'<a href="'+posturl+'">'+posttitle+'</a>'+'</div>');
+document.write('<div class="posttitle">'+'<a href="'+posturl+'">'+posttitle+'</a>=123'+'</div>');
 document.write('</span></li>'+'</ul>'+'</div>');
 
 document.write('<div>HERE:'+'<a href="'+posturl+'">'+posttitle+'</a>'+'</div>');
